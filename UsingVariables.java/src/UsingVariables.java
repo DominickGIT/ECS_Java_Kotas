d7ea3c7878a4bc30a541baafe68ce70267c10fdd
DominickGIT <KotasD7411@sduhsd.lan>
2017-12-04 10:52:16 -0800

    Added 3 Projects
    Added 3 more projects.

--- UsingVariables.java/src/UsingVariables.java
+++ UsingVariables.java/src/UsingVariables.java
@@ -1,5 +1,21 @@
 
 public class UsingVariables
 {
-
+	public static void main( String[] args ) 
+	{
+		int room;
+		double e;
+		String learning;
+		
+		room = 113;
+		e = 2.71828;
+		learning = "Computer Science";
+		 
+		
+		System.out.println("This is room number # " + room);
+		System.out.println("e is close to " + e);
+		System.out.println("I am learning a bit about " + learning);
+		
+		
+	}
 }
