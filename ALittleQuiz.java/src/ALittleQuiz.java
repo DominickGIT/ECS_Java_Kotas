f02a65eb0bae1b7a15499b993f5dc42720920381
DominickGIT <KotasD7411@sduhsd.lan>
2017-12-07 10:54:05 -0800

    uup
    date

--- ALittleQuiz.java/src/ALittleQuiz.java
+++ ALittleQuiz.java/src/ALittleQuiz.java
@@ -7,6 +7,7 @@ public class ALittleQuiz
 		
 		String names; 
 		int answer;
+		double finalanswer;
 		
 		System.out.println("Q1) What is the capital of Alaska?");
 		System.out.println("     1) Melbourne");
@@ -24,10 +25,41 @@ public class ALittleQuiz
 	
 		
 		System.out.println("Q2) Can you store the value cat in a variable of type int?");
-		System.out.println("1) yes ");
-		System.out.println("2) no ");
+		System.out.println("     1) yes ");
+		System.out.println("     2) no ");
+		answer = keyboard.nextInt();
 		
+		if ( answer <= 1 )
+		{
+			System.out.println("Sorry, cat is a string. ints can only store numbers.");
+		}
+		else if ( answer <= 2 )
+		{
+			System.out.println("Yes! That's correct.");
+		}
+		System.out.println("Q3) What is the result of 9+6/3?");
+		System.out.println("     1) 5");
+		System.out.println("     2) 11");
+		System.out.println("     3) 15/3");
+		answer = keyboard.nextInt();
 		
+		if ( answer <= 1 )
+		{
+			System.out.println("Sorry, that's not correct.");
+		}
+		else if ( answer <= 2 )
+		{
+			System.out.println("Yes! Good job that's correct!");
+		}
+		else if ( answer <= 3 )
+		{
+			System.out.println("Sorry, that's not correct.");
+		}
+		
+		finalanswer = answer/3;
+		
+		System.out.println("Overall, you got " + finalanswer + "/3 correct.");
+		System.out.println("Thanks for playing!");
 		
 	}
 }
